6307d5d64d784236bd2985b7da9133a6c9ea3790
Jan Balangue <jantonio.balangue@gmail.com>
2018-02-21 13:27:20 -0800

    Modified style.css and game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         this.healthPoints = 0;
         this.attackPower = 0;
         this.counterAttackPower = 0;
+        this.picture = "";
     };
 
     var fighter = [];
@@ -14,22 +15,27 @@ $(document).ready(function () {
     fighter[0] = new character();
     fighter[0].name = "Luke Skywalker";
     fighter[0].counterAttackPower = 5;
+    fighter[0].picture = '<img src="assets/images/luke-skywalker.JPG">';
 
     fighter[1] = new character();
     fighter[1].name = "Rey";
     fighter[1].counterAttackPower = 10;
+    fighter[1].picture = '<img src="assets/images/rey.JPG">';
 
     fighter[2] = new character();
     fighter[2].name = "Kylo Ren";
     fighter[2].counterAttackPower = 15;
+    fighter[2].picture = '<img src="assets/images/kylo-ren.JPG">';
 
     fighter[3] = new character();
     fighter[3].name = "Darth vader";
     fighter[3].counterAttackPower = 20;
+    fighter[3].picture = '<img src="assets/images/darth-vader.JPG">';
 
     var yourCharacter = {};
     var yourCharacterDiv;
     var defender = {};
+    var enemyDiv = [];
 
     function initialize() {
         // reset healthPoints and attackPower for each fighter
@@ -63,6 +69,17 @@ $(document).ready(function () {
         console.log(yourCharacterDiv);
 
         $("#your-character").html(yourCharacterDiv);
+
+        // create divs for all enemies
+        for (var i = 0; i < fighter.length; i++) {
+            if (fighter[i].name !== yourCharacter.name) {
+                // create enemy div and append to #enemies-to-attack
+                enemyDiv[i] = $("<div>");
+                enemyDiv[i].html('<figure class="enemy"><figcaption class="enemy-name">' + yourCharacter.name +
+                '</figcaption>' + fighter[i].picture + '<figcaption class="enemy-health-points">' + fighter[i].healthPoints + '</figcaption>');
+                $("#enemies-to-attack").append(enemyDiv[i]);
+            }
+        }
     }
 
     initialize();
