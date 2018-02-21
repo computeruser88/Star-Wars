1bfd681152402321acc95023c9cb28af2a714221
Jan Balangue <jantonio.balangue@gmail.com>
2018-02-20 20:55:16 -0800

    Modified style.css and game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+$document.ready(function () {
+
+    function character() {
+        this.name = "";
+        this.healthPoints = 0;
+        this.attackPower = 0;
+        this.counterAttackPower = 0;
+        this.picture = "";
+    };
+
+    var fighter = [];
+
+    fighter[0] = new character();
+    fighter[0].name = "Luke Skywalker";
+    fighter[0].healthPoints = 100;
+    fighter[0].attackPower = 7;
+    fighter[0].counterAttackPower = 5;
+    fighter[0].picture = "../images/luke-skywalker.JPG";
+
+    fighter[1] = new character();
+    fighter[1].name = "Rey";
+    fighter[1].healthPoints = 120;
+    fighter[1].attackPower = 8;
+    fighter[1].counterAttackPower = 10;
+    fighter[1].picture = "../images/rey.JPG";
+
+    fighter[2] = new character();
+    fighter[2].name = "Kylo Ren";
+    fighter[2].healthPoints = 150;
+    fighter[2].attackPower = 9;
+    fighter[2].counterAttackPower = 20;
+    fighter[2].picture = "../images/kylo-ren.JPG";
+
+    fighter[3] = new character();
+    fighter[3].name = "Darth Vader";
+    fighter[3].healthPoints = 180;
+    fighter[3].attackPower = 10;
+    fighter[3].counterAttackPower = 25;
+    fighter[3].picture = "../images/darth-vader.JPG";
+});
