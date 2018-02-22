1d1ea016ffb9fdcd8800caf1d3da0ebc7e16e212
Jan Balangue <jantonio.balangue@gmail.com>
2018-02-21 17:27:29 -0800

    Modified style.css and game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
         this.attackPower = 0;
         this.counterAttackPower = 0;
         this.picture = "";
+        this.isDead = false;
     };
 
     var fighter = [];
     var enemies = [];
     var yourPicture;
+    var defender = {};
 
     fighter[0] = new character();
     fighter[0].name = "Luke Skywalker";
@@ -24,17 +26,18 @@ $(document).ready(function () {
 
     fighter[2] = new character();
     fighter[2].name = "Kylo Ren";
-    fighter[2].counterAttackPower = 15;
+    fighter[2].counterAttackPower = 20;
     fighter[2].picture = '<img src="assets/images/kylo-ren.JPG">';
 
     fighter[3] = new character();
     fighter[3].name = "Darth vader";
-    fighter[3].counterAttackPower = 20;
+    fighter[3].counterAttackPower = 25;
     fighter[3].picture = '<img src="assets/images/darth-vader.JPG">';
 
     var yourCharacter = {};
-    var yourCharacterDiv;
+    var yourCharacterDiv = $("<div>");
     var defender = {};
+    var defenderDiv = $("<div>");
     var enemyDiv = [];
 
     function initialize() {
@@ -50,6 +53,10 @@ $(document).ready(function () {
 
         fighter[3].healthPoints = 180; // Vader
         fighter[3].attackPower = 11;
+
+        defender = {};
+        yourCharacter = {};
+        enemies.length = 0;
     }
     function createAllCharacters(yourCharacter, yourPicture) {
         console.log("yourCharacter " + yourCharacter);
@@ -62,10 +69,10 @@ $(document).ready(function () {
                 console.log("Enemy " + enemies);
             }
         }
-        yourCharacterDiv = $("<div>");
+
 
         yourCharacterDiv.html('<figure id="you"><figcaption id="your-name">' + yourCharacter.name +
-            '</figcaption>' + yourPicture + '<figcaption>' + yourCharacter.healthPoints + '</figcaption>');
+            '</figcaption>' + yourPicture + '<figcaption id="your-health-points">' + yourCharacter.healthPoints + '</figcaption>');
         console.log(yourCharacterDiv);
 
         $("#your-character").html(yourCharacterDiv);
@@ -77,23 +84,70 @@ $(document).ready(function () {
             enemyDiv[i].html('<figure class="enemy"><figcaption class="enemy-name">' + enemies[i].name +
                 '</figcaption>' + enemies[i].picture + '<figcaption class="enemy-health-points">' + enemies[i].healthPoints + '</figcaption>');
             $("#enemies-to-attack").append(enemyDiv[i]);
-
+            enemies[i].isDead = false;
         }
+    }
+
+    function chooseFirstDefender() {
+        $("#enemies-to-attack").on("click", enemyDiv[0], function () {
+            enemyDiv[0].remove();
+            defenderDiv.html('<figure class="defender"><figcaption class="defender-name">' + enemies[0].name +
+                '</figcaption>' + enemies[0].picture + '<figcaption class="defender-health-points">' + enemies[0].healthPoints + '</figcaption>');
+            $("#defender").append(defenderDiv);
+            defender = enemies[0];
+        }).on("click", enemyDiv[1], function () {
+            enemyDiv[1].remove();
+            defenderDiv.html('<figure class="defender"><figcaption class="defender-name">' + enemies[1].name +
+                '</figcaption>' + enemies[1].picture + '<figcaption class="defender-health-points">' + enemies[1].healthPoints + '</figcaption>');
+            $("#defender").append(defenderDiv);
+            defender = enemies[1];
+        }).on("click", enemyDiv[2], function () {
+            enemyDiv[2].remove();
+            defenderDiv.html('<figure class="defender"><figcaption class="defender-name">' + enemies[2].name +
+                '</figcaption>' + enemies[2].picture + '<figcaption class="defender-health-points">' + enemies[2].healthPoints + '</figcaption>');
+            $("#defender").append(defenderDiv);
+            defender = enemies[2];
+        });
+        return defender;
+    }
+
+    function fight (yourCharacter, defender) {
+        $("#attack").on("click", function( {
+            // attack first, counterattack second, update message third
+            defender.healthPoints = defender.healthPoints - yourCharacter.attackPower;
+            $("#message").text("You attacked " + defender.name + " for " + yourCharacter.attackPower + " damage.");
+            yourCharacter.attackPower += 8; // increment your attackPower
+            if (defender.healthPoints <= 0) {
+                defender.isDead = true;
+            }
+            // counterattack
+            yourCharacter.healthPoints -= defender.counterAttackPower;
+            $("#your-health-points").text(yourCharacter.healthPoints);
+            if (yourCharacter.healthPoints <= 0) {
+                yourCharacter.isDead = true;
+            } else {
+                $("#message").append("<br>" + defender.name + " attacked you back for " + defender.counterAttackPower + " damage.");
+            }
+        });
     }
 
     initialize();
 
     $("#choose-character").on("click", "#choose-luke-skywalker", function () {
         yourCharacter = fighter[0];
-        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/luke-skywalker.JPG">');
+        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/luke-skywalker.JPG" alt="Luke Skywalker">');
+        fight(yourCharacter, chooseFirstDefender());
     }).on("click", "#choose-rey", function () {
         yourCharacter = fighter[1];
-        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/rey.JPG">');
+        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/rey.JPG" alt="Rey">');
+        fight(yourCharacter, chooseFirstDefender());
     }).on("click", "#choose-kylo-ren", function () {
         yourCharacter = fighter[2];
-        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/kylo-ren.JPG">');
+        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/kylo-ren.JPG" alt="Kylo Ren">');
+        fight(yourCharacter, chooseFirstDefender());
     }).on("click", "#choose-darth-vader", function () {
         yourCharacter = fighter[3];
-        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/darth-vader.JPG">');
+        createAllCharacters(yourCharacter, '<img id="your-pic" src="assets/images/darth-vader.JPG" alt="Darth Vader">');
+        fight(yourCharacter, chooseFirstDefender());
     });
 });
